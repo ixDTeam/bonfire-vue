687ebbc87f025c2c856f18e69f2d45fcb03cb5d2
Lukas <licher.lukas@gmail.com>
2019-05-23 18:21:00 +0200

    Mit Journey Falsch Herum

--- src/views/Giveaway.vue
+++ src/views/Giveaway.vue
@@ -2,8 +2,8 @@
   <div class="giveaway">
     <div class="color-bg fullscreen pad-t">
       <h1>Gib es weiter</h1>
-      <div class="giveaway-alert fail" v-if="checkStory">Your Story was not passed on yet!</div>
-      <div class="giveaway-alert success" v-if="!checkStory">Deine Story wurde weitergegeben!</div>
+      <div class="giveaway-alert fail" v-if="!checkStory">Your Story was not passed on yet!</div>
+      <div class="giveaway-alert success" v-if="checkStory">Deine Story wurde weitergegeben!</div>
       <div class="countdown">
         <countdown :time="time" :interval="100" tag="span">
           <template slot-scope="props">{{ props.days }}</template>
@@ -48,11 +48,7 @@ export default {
   },
   firestore() {
       return {
-<<<<<<< HEAD
-          story: db.collection("object/"+this.$cookies.get('id')+"/story").orderBy('created', 'desc')
-=======
           story: db.collection("object/"+this.$cookies.get('id')+"/story").doc(this.$cookies.get('ownStoryID'))
->>>>>>> style2
       };
   },
   computed:{
